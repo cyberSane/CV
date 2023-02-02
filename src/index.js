c9b086b803d91b99a4b940e2d3b6a88d587070b8
Akshay <cyberSane@users.noreply.github.com>
2023-02-02 19:38:41 +0100

    Rebuild project with slight structure change.

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import SideNav from "./components/SideNav";
 import MainContent from "./components/MainContent";
 import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
 const isFull = false;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
     <React.StrictMode>
